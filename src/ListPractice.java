e12ae4743aa33a83fd5558836f149a517a3a4224
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-01-05 18:33:38 -0800

    Completed ListPractice and added refresher in toRefresh.md

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,50 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    List<String> list = new ArrayList<>();
 
     // Add 3 elements to the list (OK to do one-by-one)
+    list.add("Dog");
+    list.add("Cat");
+    list.add("Duck");
 
     // Print the element at index 1
+    System.out.println("Element at index 1 is: " + list.get(1));
 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
+    list.set(1, "Wombat");
 
     // Insert a new element at index 0 (the length of the list will change)
+    list.add(0, "Goat");
 
     // Check whether the list contains a certain string
+    boolean containsCat = list.contains("Cat");
+    System.out.println("Does the list contain the word 'Cat'? " + containsCat); //false since the word was replaced
 
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
+    System.out.println("FOR-LOOP:");
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println("Index " + i + ": " + list.get(i));
+    }
 
     // Sort the list using the Collections library
+    Collections.sort(list);
 
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+    System.out.println("FOR-EACH LOOP:");
+    for (String word : list) {
+      System.out.println(word);
+    }
 
     /*
      * Usage tip!
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,10 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
-- 
+ListPractice "https://www.geeksforgeeks.org/arraylist-set-method-in-java-with-examples/"
+- List<String> list = new ArrayList<>(); I always get confused how to set this up
+- get() function access the element
+- set() function that replaces a word in the list
+- contains() https://www.geeksforgeeks.org/list-contains-method-in-java-with-examples/?ref=header_outind 
+- size() https://stackoverflow.com/questions/4438710/using-collection-size-in-for-loop-comparison 
+- Collections.sort() https://www.geeksforgeeks.org/collections-sort-java-examples/?ref=header_outind 
